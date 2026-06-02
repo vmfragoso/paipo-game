e5f66e6982b8ca3e0e8c75b4b29c84de616304ea
tiagogrunge <tiagogrunge@gmail.com>
2026-06-02 16:37:42 -0300

    fix: comment out unnecessary code in hit_effect function

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -5,7 +5,8 @@ extends RigidBody2D
 func hit_effect(matrix_ativo: bool):
 	print("BOLA APANHOU E FICOU BRANCA")
 	particles.emitting = true
-	$Sprite2D.modulate = Color(50, 50, 50, 1)  # white Super Brilliant pa caraleop colo.withe nao funciona acho que se eu aumentar melhora
+	# white Super Brilliant pa caraleop colo.withe nao funciona acho que se eu aumentar melhora
+	$Sprite2D.modulate = Color(50, 50, 50, 1)  
 	await get_tree().create_timer(0.2).timeout
 	$Sprite2D.modulate = Color(1, 1, 1, 1)  # back to normal
 	
